46f65cfa1eddaafd28f0a4709b85975ce36e6ed6
Phi Innovations <flavio.alves@phiinnovations.com>
2019-09-29 08:24:57 -0300

    Inclusão de remoção de histórico de tendências

--- sources/meta-cm/recipes-cm/equalizer-api/files/delete_logs.sh
+++ sources/meta-cm/recipes-cm/equalizer-api/files/delete_logs.sh
@@ -3,6 +3,7 @@
 sqlite3 /var/www/equalizer-api/equalizer-api/equalizerdb "PRAGMA busy_timeout=60000;"
 sqlite3 /var/www/equalizer-api/equalizer-api/equalizerdb "PRAGMA journal_mode=WAL;"
 sqlite3 /var/www/equalizer-api/equalizer-api/equalizerdb "DELETE FROM DataLog;"
+sqlite3 /var/www/equalizer-api/equalizer-api/equalizerdb "DELETE FROM Tendencias;"
 sqlite3 /var/www/equalizer-api/equalizer-api/equalizerdb "DELETE FROM AlarmLog;"
 sqlite3 /var/www/equalizer-api/equalizer-api/equalizerdb "VACUUM;"
 reboot
